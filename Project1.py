6f2041b78672bb860520e12fe126b020797b9f39
willsanthescienceman <coconn20@syr.edu>
2025-09-23 16:07:32 -0400

    added imports and msg() for debugging

--- Project1.py
+++ Project1.py
@@ -1,1 +1,16 @@
+# Claire O'Connor
+# PHY607: Project 1
+
+import numpy as np
+import matplotlib.pyplot as plt
+import main_EM as EM
+import main_SHM as SHM
+
+# Debugging
+msg_lvl = 5
+def msg(lvl, msg):
+    if msg_lvl < lvl:
+        print(msg)
+
+
 
