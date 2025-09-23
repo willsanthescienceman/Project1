e0d0d0a839eba15f47044ccd1a12c9fc84610267
willsanthescienceman <coconn20@syr.edu>
2025-09-23 15:32:14 -0400

    Updated module to include 4th order, changed some function names to match mathematical documentation, added comments and labels for clarity. Removed multiplication by step size from update_value() (previously named update_y())

--- rungekutta.py
+++ rungekutta.py
@@ -1,26 +1,53 @@
-# Claire O'Connor, Amelia Abruscato & Shreyan Goswami
-# Runge Kutta
+# 4th Order Runge Kutta
 import numpy as np
 
-def calc_next_step(y, t, step=0.1, mass=10, k=1):
-    k1 = compute_derivaties(y, t, mass, k)
-    k2 = compute_k2(y, t, step, k1, mass, k)
-    return update_y(y, k1, k2, step)
+# --- USER MANAGED --- #
+def acc(pos, mass, k):
+    return -k*pos/mass
 
-def compute_derivaties(y, t, mass, k):
-    derivative = [y[1]] # velocity, dy_0/dt
-    derivative.append(-k*y[0]/mass) # acceleration, dy_1/dt
-    return derivative
+# --- COEFFICIENTS --- #
 
-def compute_k2(y, t, step, k_1, mass, k):
+def k_1(y, t, mass, k):
+    f = [y[1]] # velocity, dy_0/dt
+    f.append(acc(y[0], mass, k)) # acceleration, dy_1/dt
+    return f
+
+def k_2(y, t, step, k_1, mass, k):
     yaux = [y[0]+step * k_1[0]/2]
     yaux.append(y[1]+step * k_1[1]/2)
     return compute_derivaties(yaux, t+step/2, mass, k)
 
-def update_y(y, k_1, k_2, step):
-    y[0] = y[0] + step*(k_1[0] + 2*k_2[0])/6 # new position
-    y[1] = y[1] + step*(k_1[1] + 2*k_2[1])/6 # new velocity
+def k_3(y, t, step, k_2, mass, k):
+    yaux = [y[0]+step * k_2[0]/2]
+    yaux.append(y[1]+step * k_2[1]/2)
+    return compute_derivaties(yaux, t+step/2, mass, k)
+
+def k_4(y, t, step, k_3, mass, k):
+    yaux = [y[0]+step * k_3[0]]
+    yaux.append(y[1]+step * k_3[1])
+    return compute_derivaties(yaux, t+step/2, mass, k)
+
+# --- UPDATING VALUES --- #
+
+# Updates the y array
+def update_y(y, t, step=0.1, mass=10, k=1):
+    # Get coefficients
+    k1 = k_1(y, t, mass, k)
+    k2 = k_2(y, t, step, k1, mass, k)
+    k3 = k_3(y, t, step, k2, mass, k)
+    k4 = k_4(y, t, step, k3, mass, k)
+
+    # Update each index in y
+    y[0] = update_value(y[0], k_1, k_2, k_3, k_4, step)
+    y[1] = update_value(y[1], k_1, k_2, k_3, k_4, step)
+    
     return y
+
+# Updates a single value (one index of the y array)
+def update_value(y_index, k_1, k_2, k_3, k_4, step):
+    return (y[0] + (k_1[0] + 2*k_2[0] + 2*k_3[0] + k4)/6)
+
+# --- CREATE ARRAY --- #
     
 def RungeKutta(pos_0, vel_0, step_size=0.1, max_time=10, mass=10, k=1):
 
@@ -33,11 +60,10 @@ def RungeKutta(pos_0, vel_0, step_size=0.1, max_time=10, mass=10, k=1):
     time_arr = time_arr * step_size
 
     # Applying Runge Kutta approximation
-    pos_arr = [y[0]]
-    vel_arr = [y[1]]
-    PE_arr = [-k * x_0**2]
-    KE_arr = [0.5 * mass * vel_0**2]
-    #total_energy = [KE_arr[0] + PE_arr[0]]
+    pos_arr = []
+    vel_arr = []
+    PE_arr = []
+    KE_arr = []
     total_energy = []
     for time in time_arr:
         y = calc_next_step(y, time)
